ed2b36d457e939eddea3d1b13d125a0a1d00b57c
Alkaponees <80576881+Alkaponees@users.noreply.github.com>
2023-04-08 10:13:05 +0300

    Update install_jenkins.sh

--- bash/install_jenkins.sh
+++ bash/install_jenkins.sh
@@ -4,13 +4,16 @@
 sudo apt update
 
 # Install Java Development Kit (JDK)
-sudo apt install openjdk-11-jdk -y
+sudo apt install  fontconfig openjdk-11-jdk -y
 
 # Add the Jenkins repository key
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # Add the Jenkins repository to the system
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update the package list to include Jenkins
 sudo apt update

1ebccec7d0a438e7a216e4c05336c30b998b913d
Alkaponees <mr.sergey1898@gmail.com>
2023-04-08 12:45:27 +0300

    Using database from remote department

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -40,48 +40,48 @@ resource "aws_security_group" "web" {
     "Name" = "Web_SG"
   }
 }
-resource "aws_security_group" "TFDefault" {
-  name   = "TFDefault_Security_Group"
-  vpc_id = aws_default_vpc.default.id
+# resource "aws_security_group" "TFDefault" {
+#   name   = "TFDefault_Security_Group"
+#   vpc_id = aws_default_vpc.default.id
 
-  dynamic "ingress" {
-    for_each = ["3306","22"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   dynamic "ingress" {
+#     for_each = ["3306","22"]
+#     content {
+#       from_port   = ingress.value
+#       to_port     = ingress.value
+#       protocol    = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    "Name" = "TFDefault_SG"
-  }
-}
-resource "aws_instance" "MySQL_instance"{
-    ami = "ami-09e1162c87f73958b"
-    instance_type = "t3.micro"
-    key_name = "Stockholm_RSA"
-    tags = {
-      "Name" = "MYSQL_Server"
-    }
-    user_data = "${file("../bash/install_MYSQL.sh")}"
-    vpc_security_group_ids = [aws_security_group.TFDefault.id]
-    ebs_block_device {
-      device_name = "/dev/sda1"
-      volume_type ="gp3"
-      volume_size = 20
-      encrypted = true
-      delete_on_termination = true
+#   tags = {
+#     "Name" = "TFDefault_SG"
+#   }
+# }
+# resource "aws_instance" "MySQL_instance"{
+#     ami = "ami-09e1162c87f73958b"
+#     instance_type = "t3.micro"
+#     key_name = "Stockholm_RSA"
+#     tags = {
+#       "Name" = "MYSQL_Server"
+#     }
+#     user_data = "${file("../bash/install_MYSQL.sh")}"
+#     vpc_security_group_ids = [aws_security_group.TFDefault.id]
+#     ebs_block_device {
+#       device_name = "/dev/sda1"
+#       volume_type ="gp3"
+#       volume_size = 20
+#       encrypted = true
+#       delete_on_termination = true
 
-    }
-}
+#     }
+# }
 
 resource "aws_launch_configuration" "web" {
   name_prefix     = "Web server configuration"
--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,7 +1,3 @@
 output "load_balancer_url" {
   value = aws_elb.web.dns_name
 }
-output "MySQL_instance_ip" {
-    value=aws_instance.MySQL_instance.public_ip
-  
-}
